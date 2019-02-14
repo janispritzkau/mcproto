d806b68006c3cc542f9da4aab17d4859db0f94ca
Janis Pritzkau <janispritzkau@gmail.com>
2019-02-14 15:25:32 +0100

    Update example

--- examples/proxy.ts
+++ examples/proxy.ts
@@ -1,38 +1,41 @@
 import { createServer, connect } from "net"
 import { Connection, PacketWriter } from "../lib"
+import { readFileSync } from "fs"
+import * as path from "path"
+import { homedir } from "os";
 
-const username = "username"
-const ign_username = "username"
-const password = "password"
+const host = "2b2t.org"
+const port = 25565
 
-const host = "localhost"
-const port = 25566
+const profiles = JSON.parse(
+    readFileSync(path.resolve(homedir(), ".minecraft/launcher_profiles.json"), "utf-8")
+)
+const { accessToken, ...account } = profiles.authenticationDatabase[profiles.selectedUser.account]
+const name = account.profiles[profiles.selectedUser.profile].displayName
+const profile = profiles.selectedUser.profile
 
 createServer(async serverSocket => {
     serverSocket.on("error", err => console.error(err.message))
     const server = new Connection(serverSocket, { isServer: true })
 
     const handshake = await server.nextPacket
-    const response = server.nextPacketWithId(0x0)
 
     const clientSocket = connect({ host, port }, async () => {
         clientSocket.on("end", () => serverSocket.end())
         serverSocket.on("end", () => clientSocket.end())
 
-        const client = new Connection(clientSocket, { username, password })
+        const client = new Connection(clientSocket, { accessToken, profile })
 
         const protocol = handshake.readVarInt()
         const nextState = (handshake.readString(), handshake.readInt16(), handshake.readVarInt())
 
-        client.send(new PacketWriter(0).writeVarInt(protocol)
+        client.send(new PacketWriter(0x0).writeVarInt(protocol)
         .writeString(host).writeUInt16(port).writeVarInt(nextState))
-        if (nextState == 2) console.log((await response).readString())
-        client.send(nextState == 2 ? new PacketWriter(0x0).writeString(ign_username) : new PacketWriter(0x0))
+        client.send(nextState == 2 ? new PacketWriter(0x0).writeString(name) : new PacketWriter(0x0))
 
         if (nextState == 2) {
             client.onPacket = packet => {
-                console.log(packet.id)
-                if (packet.id == 0x02) {
+                if (packet.id == 0x2) {
                     console.log(packet.readString(), packet.readString())
                     server.send(packet)
                 }
@@ -43,7 +46,6 @@ createServer(async serverSocket => {
         client.onPacket = packet => server.send(packet)
         server.onPacket = packet => {
             if (packet.id == 0xb) return
-            console.log(packet.id.toString(16))
             client.send(packet)
         }
     })
