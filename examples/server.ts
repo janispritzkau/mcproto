f1372db3372ad0347b1336496dcdf6e358423b38
Janis Pritzkau <janispritzkau@gmail.com>
2019-02-28 09:35:22 +0100

    Simplify example

--- examples/server.ts
+++ examples/server.ts
@@ -1,30 +1,28 @@
 import { createServer } from "net"
-import { Connection, PacketWriter, PacketReader } from "../lib"
+import { Connection, PacketWriter, State } from ".."
 
 createServer(async socket => {
     socket.on("error", err => console.error(err.message))
     const client = new Connection(socket)
     client.onError = error => console.log(error.message)
 
-    let packet: PacketReader
+    const handshake = await client.nextPacket()
+    const protocol = handshake.readVarInt()
 
-    packet = await client.nextPacket()
-    const protocol = packet.readVarInt()
-    packet.readString(), packet.readUInt16()
-    const nextState = packet.readVarInt()
+    if (client.state != State.Status) return socket.end()
 
-    if (nextState != 1) return socket.end()
+    await client.nextPacket()
 
     client.send(
         new PacketWriter(0x0).writeString(JSON.stringify({
-            version: { name: "node", protocol },
+            version: { name: "Node", protocol },
             players: { max: 0, online: 0 },
             description: { text: "NodeJS server", color: "light_purple" }
         }))
     )
 
     while (true) {
-        packet = await client.nextPacketWithId(0x1)
-        client.send(new PacketWriter(0x1).write(packet.read(8)))
+        const ping = await client.nextPacketWithId(0x1)
+        client.send(new PacketWriter(0x1).write(ping.read(8)))
     }
 }).listen(25565)
