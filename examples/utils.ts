c9d27ccb5ac62586d493b10570304aa279c0d2b2
Janis Pritzkau <janispritzkau@gmail.com>
2019-08-12 15:37:50 +0200

    Update examples to work on windows and mac

--- examples/utils.ts
+++ examples/utils.ts
@@ -3,7 +3,17 @@ import { homedir } from "os"
 import * as path from "path"
 
 export function getProfile() {
-    const config = JSON.parse(readFileSync(path.resolve(homedir(), ".minecraft/launcher_profiles.json"), "utf-8"))
+    let mcPath: string
+
+    if (process.platform == "win32") {
+        mcPath = path.join(homedir(), "AppData/Roaming/.minecraft")
+    } else if (process.platform == "darwin") {
+        mcPath = path.join(homedir(), "Library/Application Support/minecraft")
+    } else {
+        mcPath = path.join(homedir(), ".minecraft")
+    }
+
+    const config = JSON.parse(readFileSync(path.join(mcPath, "launcher_profiles.json"), "utf-8"))
     const { accessToken, profiles } = config.authenticationDatabase[config.selectedUser.account]
     const { profile } = config.selectedUser, { displayName } = profiles[profile]
 
